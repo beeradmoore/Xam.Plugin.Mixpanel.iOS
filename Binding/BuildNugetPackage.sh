1f494ba107a18ade3a579f2acc8560eeb32ee5e3
BradM <brad@4pi.com.au>
2019-03-24 17:06:30 +1100

    Included FetchFramework request in BuildNugetPackage

--- Binding/BuildNugetPackage.sh
+++ Binding/BuildNugetPackage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
+./FetchFramework.sh
 msbuild Xam.Plugin.Mixpanel.iOS.csproj -property:Configuration=Release -target:Clean,Build
 nuget pack Xam.Plugin.Mixpanel.iOS.csproj -Properties Configuration=Release
