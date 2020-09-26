802941cabd43ebada37bb28a751c38a91941dfcd
Brad Moore <brad@4pi.com.au>
2020-09-26 15:03:35 +1000

    Updated to 3.6.1.

--- Binding/BuildNugetPackage.sh
+++ Binding/BuildNugetPackage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./FetchFramework.sh
 msbuild Xam.Plugin.Mixpanel.iOS.csproj -property:Configuration=Release -target:Clean,Build
-nuget pack Xam.Plugin.Mixpanel.iOS.csproj -Properties Configuration=Release
+nuget pack Xam.Plugin.Mixpanel.iOS.nuspec -Symbols -SymbolPackageFormat snupkg
