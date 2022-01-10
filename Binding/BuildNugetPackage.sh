6f67ba1f5e6e21db8e10f5d428a18dc39314f4d8
Brad Moore <brad@4pi.com.au>
2022-01-10 16:59:22 +1100

    Added a check to make sure we don't try to build the nuget package with incorrect/out of date frameworks inside it.

--- Binding/BuildNugetPackage.sh
+++ Binding/BuildNugetPackage.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 ./FetchFramework.sh
+
+if [ $? -ne 0 ]
+then
+   echo "ERROR: Unable to fetch Mixpanel framework"
+   exit 1
+fi
+
 msbuild Xam.Plugin.Mixpanel.iOS.csproj -property:Configuration=Release -target:Clean,Build
 nuget pack Xam.Plugin.Mixpanel.iOS.nuspec -Symbols -SymbolPackageFormat snupkg
--- Binding/FetchFramework.sh
+++ Binding/FetchFramework.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
 carthage update
+
+# If carthage failed.
+if [ $? -ne 0 ]
+then
+   exit 1
+fi
+
 cp -r Carthage/Build/iOS/Mixpanel.framework .
+
+# If we couldn't copy the Mixpanel.framework.
+if [ $? -ne 0 ]
+then
+   exit 1
+fi
+
 rm -rf Carthage
 
